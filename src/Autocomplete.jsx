038d02e34637cf3669a071490e27a2d3c5906c67
roxanaromocea <roxanaromocea@gmail.com>
2019-09-09 16:25:42 +0300

    Add debounce to input

--- src/Autocomplete.jsx
+++ src/Autocomplete.jsx
@@ -1,32 +1,42 @@
 import React from 'react';
+import debounce from 'lodash.debounce';
 
 export default class Autocomplete extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            filteredSuggestions: [],
-            selectedSuggestion: 0
+            filteredSuggestions: null,
+            selectedSuggestion: null,
+            value: props.value
         };
 
         this.onInputChange = this.onInputChange.bind(this);
         this.onSuggestionClicked = this.onSuggestionClicked.bind(this);
         this.onKeyDown = this.onKeyDown.bind(this);
+        this.callPropsChangeMehod = this.callPropsChangeMehod.bind(this);
+        this.callPropsChangeMehod = debounce(this.callPropsChangeMehod, props.debounceTime);
+    }
+
+    callPropsChangeMehod(value) {
+        this.props.onChange && this.props.onChange(value);
     }
 
     onInputChange(e) {
-        let filteredSuggestions = [];
+        let filteredSuggestions = null;
         const inputValue = e.target.value || '';
 
-        filteredSuggestions = this.props.suggestions.filter(suggestion => (
+        filteredSuggestions = inputValue && this.props.suggestions.filter(suggestion => (
             suggestion.toLowerCase().indexOf(inputValue.toLowerCase()) > -1
         ))
 
         this.setState({
-            filteredSuggestions
+            filteredSuggestions,
+            value: inputValue
         });
 
-        this.props.onChange(inputValue);
+        this.callPropsChangeMehod(inputValue);
+        
     }
 
     onKeyDown(e) {
@@ -36,45 +46,55 @@ export default class Autocomplete extends React.Component {
         //arrow down key
         if(keyCode === 40 && selectedSuggestion < filteredSuggestions.length - 1) {
             this.setState((state) => ({
-                selectedSuggestion: state.selectedSuggestion + 1
+                value: state.filteredSuggestions[state.selectedSuggestion + 1],
+                selectedSuggestion: state.selectedSuggestion + 1,
             }))
         }
         
         //arrow up key 
         if(keyCode === 38 && selectedSuggestion !== 0 ) {
             this.setState((state) => ({
-                selectedSuggestion: state.selectedSuggestion - 1
+                value: state.filteredSuggestions[state.selectedSuggestion - 1],
+                selectedSuggestion: state.selectedSuggestion - 1,
             }))
         }
 
         //enter key
         if(keyCode === 13) {
             this.setState({
-                filteredSuggestions: [],
-                selectedSuggestion: 0
+                filteredSuggestions: null,
+                selectedSuggestion: 0,
             });
-            this.props.onChange(this.state.filteredSuggestions[this.state.selectedSuggestion]);
+            this.callPropsChangeMehod(this.state.filteredSuggestions[this.state.selectedSuggestion]);
             e.preventDefault();
         }
+
     } 
 
     onSuggestionClicked(e) {
         this.setState({
-            filteredSuggestions: []
+            filteredSuggestions: [],
+            value: e.target.innerText
         });
-        this.props.onChange(e.target.innerText);
+        this.callPropsChangeMehod(e.target.innerText);
+    }
+
+    onSuggestionMouseEnter(index) {
+        this.setState({
+            selectedSuggestion: index
+        })
     }
 
     render() {
-        const { wrapperClassName, inputClassName, autocompleteClassName } = this.props;
+        const { containerStyle, inputStyle, autocompleteStyle, debounceTime } = this.props;
         return (
-            <div className={wrapperClassName}>
-                <input type="text" value={this.props.value}
+            <div style={containerStyle}>
+                <input type="text" value={this.state.value}
                     onChange={this.onInputChange}
                     onKeyDown={this.onKeyDown}
-                    className={inputClassName}
+                    style={inputStyle}
                 />
-                <div className={autocompleteClassName}>
+                <div style={autocompleteStyle}>
                     {this.renderAutocomplete()}
                 </div>
             </div>
@@ -82,18 +102,25 @@ export default class Autocomplete extends React.Component {
     }
 
     renderAutocomplete() {
-        const { filteredSuggestions } = this.state;
+        const { filteredSuggestions, selectedSuggestion } = this.state;
+        const { selectedSuggestionStyle } = this.props;
 
         return (
             filteredSuggestions ?
-                filteredSuggestions.map((suggestion, index) => (
-                    <div key={index}
-                        onClick={this.onSuggestionClicked}
-                        className={`${this.state.selectedSuggestion === index ? 'selected' : ''} suggestion`}
-                    >
-                        {suggestion}
-                    </div>
-                ))
+                filteredSuggestions.map((suggestion, index) => {
+                    const selectedStyle = selectedSuggestion === index ? selectedSuggestionStyle : null;
+
+                    return(
+                        <div key={index}
+                            onClick={this.onSuggestionClicked}
+                            onMouseEnter={() => this.setState({ selectedSuggestion: index })}
+                            onMouseLeave={() => this.setState({ selectedSuggestion: null })}
+                            style={selectedStyle}
+                        >
+                            {suggestion}
+                        </div>
+                    )
+                })
                 :
                 null
         );
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -5,7 +5,27 @@ import { storiesOf } from '@storybook/react';
 import Autocomplete from '../src';
 
 
+
 storiesOf('Autocomplete', module)
-    .add('simple autocomplete', () => (
-        <Autocomplete suggestions={['1', '2', '3']} value={4} />
-));
+    .add('simple autocomplete', () => {
+        const containerStyle = {
+            width: "200px",
+            display: "flex",
+            flexDirection: "column"
+        };
+        const selectedSuggestionStyle = {
+            backgroundColor: "#cce6ff"
+        };
+        const autocompleteStyle = {
+            width: "inherit",
+            border: "1px solid #ccc",
+            cursor: "pointer"
+        };
+
+        return (<Autocomplete suggestions={['1', '2', '3', '33', '22', 'roxana', 'romana']}
+                              value={4}
+                              debounceTime={2000}
+                              containerStyle={containerStyle}
+                              autocompleteStyle={autocompleteStyle}
+                              selectedSuggestionStyle={selectedSuggestionStyle}/>)
+});
